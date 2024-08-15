a13405067e7e1674f4a07fdc59e2f9a40506e4a9
Austin Archer <aarcher@datalinknetworks.net>
2024-08-14 23:29:51 -0700

    Better processing of choco list output

--- pkg/choco/packages.go
+++ pkg/choco/packages.go
@@ -1,8 +1,10 @@
 package choco
 
 import (
+	"bytes"
 	"fmt"
 	"os/exec"
+	"strconv"
 	"strings"
 
 	"github.com/goodieshq/sweettooth/pkg/util"
@@ -49,65 +51,122 @@ func pkgactionName(action PkgAction) string {
 	}
 }
 
-func ListAllInstalled() ([]util.Software, []util.Software, error) {
+type partType int
+
+const (
+	TYPE_UNKNOWN partType = iota
+	TYPE_CHOCO   partType = iota
+	TYPE_SYSTEM  partType = iota
+)
+
+// convert an arbitrarily delimited lines into util.Software name/version
+func parseSoftwareList(lines []string, delim string) util.SoftwareList {
+	var software util.SoftwareList
+	for _, line := range lines {
+		parts := strings.Split(line, delim)
+		if len(parts) != 2 { // line is unexpected
+			log.Warn().Str("line", line).Msg("invalid line")
+			continue
+		}
+		software = append(software, util.Software{Name: parts[0], Version: parts[1]})
+	}
+	return software
+}
+
+func getListAllInstalledPart(part string) (util.SoftwareList, partType) {
+	lines := strings.Split(strings.TrimSpace(part), "\n")
+	if len(lines) > 0 {
+		if strings.HasSuffix(strings.ToLower(lines[len(lines)-1]), "packages installed.") {
+			return parseSoftwareList(lines[0:len(lines)-1], " "), TYPE_CHOCO
+		}
+		if strings.HasSuffix(strings.ToLower(lines[len(lines)-1]), "applications not managed with chocolatey.") {
+			return parseSoftwareList(lines[0:len(lines)-1], "|"), TYPE_SYSTEM
+		}
+	}
+	return nil, TYPE_UNKNOWN
+}
+
+func ListAllInstalled() (util.SoftwareList, util.SoftwareList, error) {
 	cmd := exec.Command("choco", "list", "--include-programs")
 	output, err := cmd.Output()
 	if err != nil {
 		return nil, nil, err
 	}
 
-	// split the output into two sections, one managed by chocolatey and the other not
+	// strip the first line, it only causes problems
+	if bytes.HasPrefix(output, []byte("Chocolatey ")) {
+		if i := bytes.IndexByte(output, '\n'); i >= 0 {
+			output = output[i+1:]
+		}
+	}
+
+	// split the output into sections separated by two newlines
 	parts := strings.Split(strings.ReplaceAll(string(output), "\r\n", "\n"), "\n\n")
+
 	if len(parts) < 2 {
 		return nil, nil, fmt.Errorf("unexpected output '%s'", string(output))
 	}
 
-	// parts[0] contains a space-delimited CSV of "name version"
-	var packageLinesChoco []string
+	var pkgChoco, pkgSystem util.SoftwareList = nil, nil
 
-	for _, line := range strings.Split(strings.TrimSpace(parts[0]), "\n") {
-		packageLinesChoco = append(packageLinesChoco, strings.TrimSpace(line))
-	}
-	if len(packageLinesChoco) > 2 &&
-		strings.HasPrefix(packageLinesChoco[0], "Chocolatey") &&
-		strings.HasSuffix(packageLinesChoco[len(packageLinesChoco)-1], " packages installed.") {
-		packageLinesChoco = packageLinesChoco[1 : len(packageLinesChoco)-1] // remove the first and last line
-	} else {
-		log.Warn().Str("output", string(output)).Msg("No choco packages or output is bad")
-		packageLinesChoco = []string{}
-	}
-
-	var packageLinesSystem []string
-
-	// parts[1] contains a pipe-delimited CSV of "name|version"
-	for _, line := range strings.Split(strings.TrimSpace(parts[1]), "\n") {
-		packageLinesSystem = append(packageLinesSystem, strings.TrimSpace(line))
-	}
-	if len(packageLinesSystem) > 1 &&
-		strings.HasSuffix(packageLinesSystem[len(packageLinesSystem)-1], " applications not managed with Chocolatey.") {
-		log.Info().Msg("YES!")
-		packageLinesSystem = packageLinesSystem[:len(packageLinesSystem)-1] // remove the first and last line
-	} else {
-		log.Warn().Str("output", string(output)).Msg("No system packages or output is bad")
-		packageLinesSystem = []string{}
-	}
-
-	// convert an arbitrarily delimited lines into util.Software name/version
-	process := func(lines []string, delim string) []util.Software {
-		var software []util.Software
-		for _, line := range lines {
-			parts := strings.Split(line, delim)
-			if len(parts) != 2 { // line is unexpected
-				log.Warn().Str("line", line).Msg("invalid line")
-				continue
-			}
-			software = append(software, util.Software{Name: parts[0], Version: parts[1]})
+	for _, part := range parts {
+		software, partType := getListAllInstalledPart(part)
+		switch partType {
+		case TYPE_CHOCO:
+			pkgChoco = software
+		case TYPE_SYSTEM:
+			pkgSystem = software
+		default:
+			continue
 		}
-		return software
 	}
 
-	// return the processed output
-	return process(packageLinesChoco, " "), process(packageLinesSystem, "|"), nil
+	if pkgChoco == nil {
+		return nil, nil, fmt.Errorf("unable to find choco output")
+	}
+
+	if pkgSystem == nil {
+		return nil, nil, fmt.Errorf("unable to find system output")
+	}
+
+	return pkgChoco, pkgSystem, nil
+}
+
+func ListChocoOutdated() ([]util.SoftwareOutdated, error) {
+	// choco list -r
+	cmd := exec.Command("choco", "outdated", "-r")
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return nil, err
+	}
+
+	var packages []util.SoftwareOutdated
+	lines := strings.Split(string(output), "\n")
+
+	for i, line := range lines {
+		lines[i] = strings.TrimSpace(line)
+	}
+
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		parts := strings.Split(line, "|")
+		pinned, err := strconv.ParseBool(parts[3])
+		if err != nil {
+			continue
+		}
+
+		packages = append(packages, util.SoftwareOutdated{
+			Name:       parts[0],
+			VersionOld: parts[1],
+			VersionNew: parts[2],
+			Pinned:     pinned,
+		})
+	}
+
+	return packages, nil
 }
 
 func ListChocoInstalled() ([]util.Software, error) {
