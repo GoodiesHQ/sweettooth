2b3b55bc4e329a0e08476c53b5cd9a148a3c37bc
Austin Archer <aarcher@datalinknetworks.net>
2024-08-28 22:19:34 -0700

    moved env processing to utils

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"context"
-	"errors"
 	"os"
 	"time"
 
@@ -15,31 +13,16 @@ import (
 
 func loop() {
 	// Initialize the logger for human-friendly output
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr}).With().Caller().Logger()
+	log.Logger = log.Output(zerolog.ConsoleWriter{
+		Out:        os.Stderr,
+		TimeFormat: time.RFC3339,
+	}).With().Caller().Logger()
 
 	godotenv.Load()
 
-	connStr := os.Getenv("SWEETTOOTH_DB_URL") // backend database (postgres only for now)
-	if connStr == "" {
-		log.Fatal().Err(errors.New("missing database connection url")).Send()
-	}
+	cfg, core := openDB()
 
-	secret := os.Getenv("SWEETTOOTH_SECRET") // secret token for JWT HMAC validation
-	if secret == "" {
-		log.Fatal().Err(errors.New("missing server secret")).Send()
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancel()
-
-	core, err := server.NewCorePGX(ctx, connStr)
-	if err != nil {
-		log.Fatal().Err(err).Msg("failed to initialize the database connection")
-	}
-
-	srv, err := server.NewSweetToothServer(server.SweetToothServerConfig{
-		Secret: secret,
-	}, core)
+	srv, err := server.NewSweetToothServer(cfg, core)
 	if err != nil {
 		log.Fatal().Err(err).Send()
 	}
---
+++ cmd/server/utils.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/goodieshq/sweettooth/pkg/api/server"
+	"github.com/rs/zerolog/log"
+)
+
+func openDB() (server.SweetToothServerConfig, *server.CorePGX) {
+
+	pgHost := os.Getenv("POSTGRES_HOST")
+	if pgHost == "" {
+		pgHost = "localhost"
+	}
+
+	pgPort := os.Getenv("POSTGRES_PORT")
+	if pgPort == "" {
+		pgPort = "5432"
+	}
+
+	p, err := strconv.Atoi(pgPort)
+	if err != nil || p <= 0x00000 || p >= 0x10000 {
+		log.Fatal().Str("invalid", pgPort).Msg("invalid postgres port")
+	}
+
+	pgUser := os.Getenv("POSTGRES_USER")
+	pgPass := os.Getenv("POSTGRES_PASSWORD")
+
+	if pgUser == "" || pgPass == "" {
+		log.Fatal().Msg("POSTGRES_USER and POSTGRES_PASSWORD are required")
+	}
+
+	pgName := os.Getenv("POSTGRES_DB")
+	if pgName == "" {
+		pgName = "sweettooth"
+	}
+
+	pgConnStr := "postgres://" +
+		pgUser + ":" + pgPass + "@" + pgHost + ":" + pgPort + "/" + pgName
+
+	if pgConnStr == "" {
+		log.Fatal().Err(errors.New("missing database connection url")).Send()
+	}
+
+	secret := os.Getenv("SWEETTOOTH_SECRET") // secret token for JWT HMAC validation
+	if secret == "" {
+		log.Fatal().Err(errors.New("missing server secret")).Send()
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	core, err := server.NewCorePGX(ctx, pgConnStr)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to initialize the database connection")
+	}
+	return server.SweetToothServerConfig{
+		Secret: secret,
+	}, core
+}
